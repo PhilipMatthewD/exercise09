e5bdf1cbe12f8b0cce2bc60e888fd8729bb6a6da
SloppyJalopy <matthewprof106@gmail.com>
2024-03-12 13:34:24 -0400

    Initial commit

---
+++ src/Contacts.java
@@ -0,0 +1,12 @@
+abstract class Contacts() {
+    private String name;
+    private String email;
+    private String phone;
+
+    public Contacts(String name, String email, String phone){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+}
